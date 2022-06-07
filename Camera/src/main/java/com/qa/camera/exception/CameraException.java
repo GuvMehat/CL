3c96e4ab50f91819ca7d7e56f7b8d746ba6cf6d7
guvmehat <gurveerm@hotmail.com>
2022-06-07 20:45:39 +0100

    CL-18 #done created camera execption

--- Camera/src/main/java/com/qa/camera/exception/CameraException.java
+++ Camera/src/main/java/com/qa/camera/exception/CameraException.java
@@ -1,5 +1,12 @@
-package com.qa.camera.exception;
+package src.main.java.com.qa.camera.exception;
 
-public class CameraException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Camera not found")
+public class CameraException extends Exception {
+
+	
+	private static final long serialVersionUID = 3322179265522207925L;
 }
