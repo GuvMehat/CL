fcd66a27ef473b409a3d4a9861d54b9bfea75fcd
guvmehat <gurveerm@hotmail.com>
2022-06-07 20:39:04 +0100

    CL-16 #done created the camera repo

--- Camera/src/main/java/com/qa/camera/persistance/repo/CameraRepo.java
+++ Camera/src/main/java/com/qa/camera/persistance/repo/CameraRepo.java
@@ -1,5 +1,15 @@
-package com.qa.camera.persistance.repo;
+package src.main.java.com.qa.camera.persistance.repo;
 
-public class CameraRepo {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+
+import src.main.java.com.qa.camera.persistance.domain.*;
+
+@Repository
+public class CameraRepo extends JpaRepository<Camera, Long> {
 
 }
