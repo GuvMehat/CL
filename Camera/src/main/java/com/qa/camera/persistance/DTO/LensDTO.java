36511e16f8a107db5cfadc4f289b98e4b6fd7c67
guvmehat <gurveerm@hotmail.com>
2022-06-08 10:59:53 +0100

    CL-15 #done added geters and  seters for lens classes

--- Camera/src/main/java/com/qa/camera/persistance/DTO/LensDTO.java
+++ Camera/src/main/java/com/qa/camera/persistance/DTO/LensDTO.java
@@ -1,5 +1,19 @@
 package com.qa.camera.persistance.DTO;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class LensDTO {
+	private long id;
+
+	private int Mount;
+	
+	private long Fstop;
+
+	private int Range;
+
+	private String Brand;
 
 }
--- Camera/src/main/java/com/qa/camera/persistance/domain/Lens.java
+++ Camera/src/main/java/com/qa/camera/persistance/domain/Lens.java
@@ -1,5 +1,65 @@
 package com.qa.camera.persistance.domain;
 
-public class Lens {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+
+public class Lens {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	private int Mount;
+	
+	private long Fstop;
+
+	private int Range;
+
+	private String Brand;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public int getMount() {
+		return Mount;
+	}
+
+	public void setMount(int mount) {
+		Mount = mount;
+	}
+
+	public long getFstop() {
+		return Fstop;
+	}
+
+	public void setFstop(long fstop) {
+		Fstop = fstop;
+	}
+
+	public int getRange() {
+		return Range;
+	}
+
+	public void setRange(int range) {
+		Range = range;
+	}
+
+	public String getBrand() {
+		return Brand;
+	}
+
+	public void setBrand(String brand) {
+		Brand = brand;
+	}
+
+	
+	
 }
